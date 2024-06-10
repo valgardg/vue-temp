7f8948b1a5404ed165815b32e84573a7950bb256
valgardg <valgardo20@ru.is>
2024-06-10 15:11:23 -0400

    added: temp component

---
+++ src/components/TempComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>This is a temp component!</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
--- src/screens/Home.vue
+++ src/screens/Home.vue
@@ -2,5 +2,13 @@
     <div>
         <p>Home</p>
         <p>I love Hailey E. Young!</p>
+        <TempComponent />
     </div>
 </template>
+
+<script setup lang="ts">
+import TempComponent from '../components/TempComponent.vue';
+</script>
+
+<style scoped>
+</style>
