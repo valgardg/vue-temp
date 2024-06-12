4d7923bc71bd2f6e337f4676375ccc7649d9a419
valgardg <valgardo20@ru.is>
2024-06-12 15:38:05 -0400

    added: header to main view along with styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>
-    <RouterView />
+  <div class="d-flex flex-column main-container">
+    <AppHeader />
+    <RouterView class="router-view"/>
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import AppHeader from './components/header/AppHeader.vue';
 </script>
+
+<style scoped>
+.main-container {
+    min-height: 100vh;
+}
+.router-view {
+    flex: 1;
+}
+</style>
