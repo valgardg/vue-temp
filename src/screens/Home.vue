dc9a28a53520e03bebe996acc3a5b8a66f8992b2
valgardg <valgardo20@ru.is>
2024-06-24 16:30:05 -0400

    added: icons and icon usage to home screen

--- src/screens/Home.vue
+++ src/screens/Home.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="container d-flex flex-column justify-content-start align-items-center main-content mt-5">
-        <p>Home</p>
-        <p>I love Hailey E. Young!</p>
+        <p class="fs-1">Home</p>
+        <p class="fs-3">I love Hailey E. Young!</p>
         <TempComponent />
+        <div>
+            <p class="fs-3">My favourite stocks!</p>
+            <div class="d-flex justify-content-between">
+                <AmazonIcon />
+                <AmdIcon />
+                <AppleIcon />
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import TempComponent from '../components/TempComponent.vue';
+import AmazonIcon from '../components/icons/amazon-icon.vue';
+import AmdIcon from '../components/icons/amd-icon.vue';
+import AppleIcon from '../components/icons/apple-icon.vue';
 </script>
 
 <style scoped>
