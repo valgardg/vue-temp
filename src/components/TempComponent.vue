903c34fcaa0b6f44472fbc202ea3be85bfe011e6
valgardg <70694766+valgardg@users.noreply.github.com>
2024-12-02 13:51:54 -0500

    Added props to tempComponetn

--- src/components/TempComponent.vue
+++ src/components/TempComponent.vue
@@ -2,6 +2,7 @@
     <div class="border p-4">
         <p>This is a temp component!</p>
         <p>{{ tempCount }}</p>
+        <p>{{ tempProp }}</p>
         <button class="btn btn-primary" @click="increaseTempCount">+</button>
         <button class="btn btn-warning" @click="decreaseTempCount">-</button>
         <LoadingIndicator />
@@ -9,9 +10,18 @@
 </template>
 
 <script setup lang="ts">
+import { withDefaults } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useTempStore } from '@/stores/TempStore';
 import LoadingIndicator from './loading/LoadingIndicator.vue';
+
+const props = withDefaults(defineProps<{
+    tempProp?: string,
+}>(), {
+    tempProp: "tempValue",
+});
+    
+// access tempProp through props.tempProp.value
 
 const { increaseTempCount, decreaseTempCount } = useTempStore();
 const { tempCount } = storeToRefs(useTempStore());
